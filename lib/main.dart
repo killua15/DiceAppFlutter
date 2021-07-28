20df281f1b17808e393a027b0ac8a193b7ee4158
David Lopez <dlopezsalinas152@gmail.com>
2021-07-28 01:36:36 -0400

    DiceApp terminada

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var letfDicenumber = 1;
+  var rightDicenumber = 2;
+
+  rollDice() {
+    setState(() {
+      letfDicenumber = Random().nextInt(6) + 1;
+      rightDicenumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Row(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Padding(
+              padding: EdgeInsets.all(20),
+              child: TextButton(
+                onPressed: () => {rollDice()},
+                child: Image.asset('images/dice$letfDicenumber.png'),
+              )),
+        ),
+        Expanded(
+            flex: 2,
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: TextButton(
+                onPressed: () => {rollDice()},
+                child: Image.asset('images/dice$rightDicenumber.png'),
+              ),
+            ))
+      ],
+    ));
   }
 }
